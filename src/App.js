8fb4c695fa693cbb2e3658f240450cab9a27d556
Malcolm McCrimmon <malcolm.mccrimmon@dish.com>
2022-09-26 13:23:33 -0600

    Initial counter app w/spinning logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [current, setCurrent] = useState(0);
+
+  const increment = () => setCurrent(current + 1);
+  const decrement = () => setCurrent(current - 1);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {current}&nbsp;
+          <button onClick={increment}>+</button>
+          <button onClick={decrement}>-</button>
+        </div>
       </header>
     </div>
   );
