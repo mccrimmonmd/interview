8403de64fc009103d2aa96a865c1867cd2e03a0b
Malcolm McCrimmon <malcolm.mccrimmon@dish.com>
2022-09-26 15:50:23 -0600

    Created class-based version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import { useState } from 'react'
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [current, setCurrent] = useState(0);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {current: 0};
+  }
 
-  const increment = () => setCurrent(current + 1);
-  const decrement = () => setCurrent(current - 1);
+  increment = () => {
+    this.setState((prevState, _) => ({
+      current: prevState.current + 1
+    }));
+  }
+  decrement = () => {
+    this.setState((prevState, _) => ({
+      current: prevState.current - 1
+    }));
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          {current}&nbsp;
-          <button onClick={increment}>+</button>
-          <button onClick={decrement}>-</button>
-        </div>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            {this.state.current}&nbsp;
+            <button onClick={this.increment}>+</button>
+            <button onClick={this.decrement}>-</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
