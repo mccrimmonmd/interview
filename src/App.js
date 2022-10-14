c3cb7768689b74acf66c8c7847d8837c63c8d4a4
Malcolm McCrimmon <malcolm.mccrimmon@dish.com>
2022-10-14 13:27:05 -0600

    Changed variable name, fixed setNum bug

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [current, setCurrent] = useState(0);
+  const [num, setNum] = useState(0);
 
-  const increment = () => setCurrent(current + 1);
-  const decrement = () => setCurrent(current - 1);
+  const increment = () => setNum(current => current + 1);
+  const decrement = () => setNum(current => current - 1);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          {current}&nbsp;
+          {num}&nbsp;
           <button onClick={increment}>+</button>
           <button onClick={decrement}>-</button>
         </div>
