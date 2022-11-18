cf31ad03b62bcb3c72cfbd13bd3ab4b79ae6bb83
Malcolm McCrimmon <malcolm.mccrimmon@dish.com>
2022-11-18 15:38:34 -0700

    Empty boilerplate app for from-scratch tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import { useState } from 'react'
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  const [num, setNum] = useState(0);
-
-  const increment = () => setNum(current => current + 1);
-  const decrement = () => setNum(current => current - 1);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          {num}&nbsp;
-          <button onClick={increment}>+</button>
-          <button onClick={decrement}>-</button>
-        </div>
-      </header>
+    <div>
+      Your code goes here!
     </div>
   );
 }
